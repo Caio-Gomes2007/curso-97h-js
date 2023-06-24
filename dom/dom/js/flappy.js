51f7c591b9031a73266aa305d9d4fa3c278171eb
the evil <caiofgomesrj@gmail.com>
2023-06-24 10:07:21 -0300

    up

---
+++ dom/dom/js/flappy.js
@@ -0,0 +1,1 @@
+//givei dps eu volto
---
+++ seção 8-array/teste.js
@@ -0,0 +1,11 @@
+// const produtos = [
+//     {nome:'notebook', preco:2499,fragil:true},
+//     {nome:'ipad Pro', preco:4199,fragil:true},
+//     {nome:'copo de vidro', preco:12.49,fragil:true},
+//     {nome:'copo de plastico', preco:18.99,fragil:false}
+// ]
+// console.log(produtos.filter(function(p){
+//     return p.fragil == false
+// }))
+
+// console.log('teste')
